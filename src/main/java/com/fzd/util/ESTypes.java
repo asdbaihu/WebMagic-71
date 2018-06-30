e892c5e7911c800ae3f42a3d9d9887bad4d57811
skyrunwith <582547045@qq.com>
2018-06-30 14:55:06 +0800

    1

---
+++ src/main/java/com/fzd/util/ESTypes.java
@@ -0,0 +1,85 @@
+package com.fzd.util;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by bdfus004 on 2017/7/25.
+ */
+public class ESTypes {
+
+    public static final String ALIBABA = "alibaba";
+
+    public static final String A1688 = "a1688";
+
+    public static final String FUND_INFO = "fundinfos";
+
+    public static final String FUND_PAY_INFO = "fundpayinfos";
+
+    public static final String SS_INFO = "ssinfos";
+
+    public static final String SS_PAY_INFO = "sspayinfos";
+
+    public static final String MOBILE_INFO = "mobileinfo";
+
+    public static final String CALL_RECORDS = "callrecords";
+
+    public static final String BILL_RECORDS = "billrecords";
+
+    public static final String SMS_RECORDS = "smsrecords";
+
+    public static final String XUE_JI = "xueji";
+
+    public static final String XUE_LI = "xueli";
+
+    public static final String CITY_HOUSE="cityhouse";
+
+    public static final String TOWN_HOUSE="townhouse";
+
+    public static final String FANG_JIA = "fangjia";
+
+    public static final String SHANG_PU = "shangpu";
+
+    public static final String BASEINFO = "baseinfo";
+
+    public static final String DETAILINFO = "detailinfo";
+
+    public static final String BUSINESS = "business";
+
+    public static final String DISHONEST = "dishonest";
+
+    public static final String PENALIZE="penalize";
+
+    public static final String WDZJ="wdzj";
+
+    public static final String TAX_ORG="taxorg";
+
+    public static final String TAX_PERSON="taxperson";
+
+    public static final String REVIEW="review";
+
+    public static final String JUDGMENTS="judgments";
+
+    public static final String TAXSTATUS_INFO= "taxstatusinfo";
+
+    public static final String VEHICLE="vehicle";
+
+    public static final String JINGDONG_USER= "jingdonguser";
+    public static final String JINGDONG_ORDER= "jingdongorder";
+
+    public static final String NET_LOAN_BLACK_LIST = "netloanblacklist";
+    public static final String USER = "user";
+
+    public static final String CISREPORT = "cisreport";
+
+    public static final String ANALYSIS_CONFIGS = "analysis_configs";
+    public static final String CHEEGU = "cheegu";
+
+    public static final String DRCICMS = "drcicms";
+
+
+    public static final int DEFAUL_SIZE = 20;
+    public static final int DEFAUL_FROM = -1;
+
+}
